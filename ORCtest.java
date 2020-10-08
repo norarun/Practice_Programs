43b753a84108f5282a72b35b1f166916a2fad5f6
NORARUN <72189851+norarun@users.noreply.github.com>
2020-10-08 21:47:41 +0900

    Update ORCtest.java

--- ORCtest.java
+++ ORCtest.java
@@ -1,7 +1,42 @@
-/*Code*/
-tesseract.setLanguage("jpn");
-List<Word> wordList = tesseract.getWords(image, TessPageIteratorLevel.RIL_BLOCK);
-String str = tesseract.doOCR(image);
-        
-/*Error message*/
-ParamsModel::Incomplete line
+/**
+ *
+ * @author 2200096
+ */
+package OCR;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import net.sourceforge.tess4j.ITessAPI.TessPageIteratorLevel;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.Word;
+
+
+
+public class TestOcr {
+ 
+    public static void main(String[] args) throws IOException, TesseractException {
+ 
+        //read
+        File target = new File("src\\OCR\\receiptTrimingTOP.jpg");
+        BufferedImage image = ImageIO.read(target);
+ 
+        //analysis
+        ITesseract tesseract = new Tesseract();
+        tesseract.setDatapath("C:\\tessdata");
+        tesseract.setLanguage("jpn");           //language (now error)
+        List<Word> wordList = tesseract.getWords(image, TessPageIteratorLevel.RIL_BLOCK);
+        String str = tesseract.doOCR(image);
+ 
+        //output
+        System.out.println(wordList);
+        System.out.println(str);
+ 
+    }
+}
